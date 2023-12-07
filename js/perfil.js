c0eb44ce7a549c80491607be85559edf91b870ae
Diego <rijmjada@gmail.com>
2023-12-07 19:11:01 -0300

    up

--- js/perfil.js
+++ js/perfil.js
@@ -1,6 +1,15 @@
 
-let USER = '';
+let USER_UID = '';
+let USER_DATA = '';
 let URL_CARGAR_IMG = 'http://localhost:8080/api/upload/usuarios/';
+
+
+// Función para el evento onload
+window.onload = function () {
+    obtenerInformacionUsuario();
+};
+
+
 
 async function obtenerInformacionUsuario() {
     try {
@@ -23,12 +32,134 @@ async function obtenerInformacionUsuario() {
 
         // Obtener el nombre del usuario desde la respuesta
         const { usuario } = await response.json();
+        cargarBtnUserRol(usuario.rol);
         cargarDatosForm(usuario);
         loadImageProfile(usuario.img)
-        USER = usuario.uid;
+        USER_UID = usuario.uid;
+        USER_DATA = usuario;
 
     } catch (error) {
         console.warn(`Error al obtener información del usuario: ${error.message}`);
+    }
+}
+
+function cargarBtnUserRol(rol) {
+    const btn = document.querySelector('#btn-user-rol');
+
+    if (rol === 'empresa') {
+        btn.textContent = 'Mis publicaciones';
+    }
+    else if (rol === 'postulante') {
+        btn.textContent = 'Mis postulaciones';
+    }
+}
+
+document.querySelector('#btn-user-rol').addEventListener('click', () => {
+
+    toogleColumnas();
+    const colTarjetas = document.querySelector('#col-tarjetas');
+    colTarjetas.innerHTML = '';
+
+    const paginacion = document.querySelector('#pagination');
+
+    if (USER_DATA.ofertasAplicadas && Array.isArray(USER_DATA.ofertasAplicadas)) {
+        // Iterar sobre cada elemento en "ofertasAplicadas"
+        USER_DATA.ofertasAplicadas.forEach(async ofertaId => {
+            const data = await obtenerDatosDeOferta(ofertaId);
+            console.log(data)
+            if (data) {
+                // Crear una nueva tarjeta
+                const nuevaTarjeta = crearTarjeta(data);
+                // Agregar la tarjeta al contenedor
+                colTarjetas.appendChild(nuevaTarjeta);
+            }
+        });
+    } else {
+        console.log("La propiedad 'ofertasAplicadas' no es un array o no existe.");
+    }
+
+});
+
+function toogleColumnas() {
+    const colImgProfile = document.querySelector('#col-imgProfile');
+    colImgProfile.classList.add('d-none');
+
+    const colTarjetasProfile = document.querySelector('#col-tarjetasProfile');
+    colTarjetasProfile.classList.remove('d-none');
+}
+
+
+
+function crearTarjeta(data) {
+    // Crear elementos HTML para la nueva tarjeta
+    const nuevaTarjeta = document.createElement('div');
+    nuevaTarjeta.className = 'col-lg-5 col-12 card m-2 ';
+    nuevaTarjeta.style.height = '250px';
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    const tituloEmpresa = document.createElement('p');
+    tituloEmpresa.id = 'titulo-empresa';
+    tituloEmpresa.textContent = data.empresa;  // Reemplaza con la propiedad correcta del objeto data
+
+    const fechaPublicacion = document.createElement('p');
+    fechaPublicacion.id = 'fecha-publicacion';
+    fechaPublicacion.textContent = 'Publicado hoy';  // Puedes agregar la lógica para obtener la fecha actual
+
+    const tituloOferta = document.createElement('h5');
+    tituloOferta.className = 'card-title py-2';
+    tituloOferta.textContent = data.puesto;  // Reemplaza con la propiedad correcta del objeto data
+
+    const descripcionOferta = document.createElement('p');
+    descripcionOferta.className = 'card-text';
+    descripcionOferta.textContent = data.descripcion;  // Reemplaza con la propiedad correcta del objeto data
+
+    const ubicacionCard = document.createElement('div');
+    ubicacionCard.className = 'ubicacion-card d-flex flex-column';
+
+    const iconoGeo = document.createElement('i');
+    iconoGeo.className = 'bi bi-geo-alt';
+    iconoGeo.textContent = ` ${data.ubicacion}`;  // Reemplaza con la propiedad correcta del objeto data
+
+    const iconoAcceso = document.createElement('i');
+    iconoAcceso.className = 'bi bi-universal-access';
+    iconoAcceso.textContent = ` ${data.tipoTrabajo}`;  // Reemplaza con la propiedad correcta del objeto data
+
+    // Agregar elementos a la tarjeta
+    ubicacionCard.appendChild(iconoGeo);
+    ubicacionCard.appendChild(iconoAcceso);
+
+    cardBody.appendChild(tituloEmpresa);
+    cardBody.appendChild(fechaPublicacion);
+    cardBody.appendChild(tituloOferta);
+    cardBody.appendChild(descripcionOferta);
+    cardBody.appendChild(ubicacionCard);
+
+    nuevaTarjeta.appendChild(cardBody);
+
+    return nuevaTarjeta;
+}
+
+
+async function obtenerDatosDeOferta(uid) {
+    try {
+        const url = `http://localhost:8080/api/ofertas/${uid}`;
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) return null;
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        return null;
     }
 }
 
@@ -47,18 +178,15 @@ function cargarDatosForm(usuario = '') {
 
 function parseJwt(token) {
     let base64Url = token.split('.')[1];
-    let base64 = base64Url.replace('-', '+').replace('_', '/');
+    let base64 = base64Url.replace('-', '+').replace('_', '/', '');
     return JSON.parse(window.atob(base64));
 };
+
 
 document.querySelector('#goHome').addEventListener('click', () => {
     window.location.href = '../index.html';
 });
 
-
-window.onload = function () {
-    obtenerInformacionUsuario();
-};
 
 
 document.querySelector('#img-user').addEventListener('mouseover', () => {
@@ -118,7 +246,7 @@ async function apiRequest(file) {
         const formData = new FormData();
         formData.append('archivo', file);
 
-        const response = await fetch(`${URL_CARGAR_IMG}${USER}`, {
+        const response = await fetch(`${URL_CARGAR_IMG}${USER_UID}`, {
             method: 'PUT',
             body: formData
         });
@@ -190,3 +318,194 @@ fileInput.addEventListener('change', async function () {
     }
 });
 /** END FILE SELECT */
+
+async function actualizarPerfil(e) {
+    e.preventDefault();
+
+    const nombre = document.querySelector('#inputName').value;
+    const apellido = document.querySelector('#inputApellido').value;
+    const correo = document.querySelector('#inputCorreo').value;
+    const contraseñaActual = document.querySelector('#inputPassword').value;
+    const newPass = document.querySelector('#inputNewPass').value;
+    const confirmPass = document.querySelector('#inputConfirmNewPass').value;
+
+    const userObj = {
+        "nombre": nombre,
+        "apellido": apellido,
+        "correo": correo,
+    };
+
+    if (contraseñaActual !== '' || newPass !== '' || confirmPass !== '') {
+        handlePasswordUpdate(userObj, contraseñaActual, newPass, confirmPass);
+    } else {
+        handleProfileUpdate(userObj);
+    }
+}
+
+async function handlePasswordUpdate(userObj, contraseñaActual, newPass, confirmPass) {
+    if (newPass !== confirmPass) {
+        messageErrorCheckPassword('Las contraseñas no coinciden');
+        return;
+    }
+
+    const spinner = showSpinner();
+    try {
+        const rta = await checkPassword(contraseñaActual);
+        if (rta.ok) {
+            userObj['password'] = newPass;
+            const response = await apiRequestUpdateProfile(userObj);
+            messageErrorCheckPassword(response.message);
+        } else {
+            messageErrorCheckPassword(rta.message);
+        }
+    } catch (error) {
+        messageErrorCheckPassword(`Error en la solicitud de actualización de perfil, ${error}`);
+    } finally {
+        hideSpinner(spinner);
+    }
+}
+
+async function handleProfileUpdate(userObj) {
+    const spinner = showSpinner();
+    try {
+        const response = await apiRequestUpdateProfile(userObj);
+        if (response.ok) {
+            messageErrorCheckPassword(response.message);
+        } else {
+            messageErrorCheckPassword(response.message);
+        }
+    } catch (error) {
+        messageErrorCheckPassword(`Error en la solicitud de actualización de perfil, ${error}`);
+    } finally {
+        hideSpinner(spinner);
+    }
+}
+
+function showSpinner() {
+    const spinner = document.querySelector('.loader-container');
+    spinner.classList.remove('d-none');
+    return spinner;
+}
+
+function hideSpinner(spinner) {
+    // Ocultar el spinner después de 5 segundos
+    setTimeout(function () {
+        spinner.classList.add('d-none');
+    }, 1500);
+}
+
+function messageErrorCheckPassword(message) {
+    let parrafo = document.querySelector('#msg-inform-p');
+    parrafo.textContent = message;
+    parrafo.style.color = 'red';
+
+    let msgErroPass = document.querySelector('#msg-inform-request');
+    msgErroPass.classList.remove('d-none');  // Asegúrate de que el contenedor esté visible
+
+    // Ocultar el mensaje después de 5 segundos
+    setTimeout(function () {
+        msgErroPass.classList.add('d-none');
+    }, 2500);
+}
+
+async function checkPassword(password) {
+
+    try {
+        const token = localStorage.getItem('x-token');
+        const { uid } = parseJwt(token);
+        const formData = new FormData();
+
+        formData.append('password', password);
+
+        const response = await fetch(`http://localhost:8080/api/usuarios/checkPass/${uid}`, {
+            method: 'PUT',
+            body: formData,
+            headers: {
+                'x-token': token
+            }
+        });
+
+        // Verificar si la respuesta es exitosa (código 2xx)
+        if (response.ok) {
+            return { ok: true, message: response.message };
+        } else {
+            const errorResponse = await response.json();
+            return { ok: false, message: errorResponse.message };
+        }
+
+    } catch (error) {
+        return { ok: false, message: error.message };
+    }
+}
+
+async function apiRequestUpdateProfile(userObj) {
+    try {
+        const token = localStorage.getItem('x-token');
+        const { uid } = parseJwt(token);
+        const formData = new FormData();
+
+        if (userObj.password) {
+            formData.append('password', userObj.password);
+        }
+        formData.append('nombre', userObj.nombre);
+        formData.append('apellido', userObj.apellido);
+        formData.append('correo', userObj.correo);
+
+        const response = await fetch(`http://localhost:8080/api/usuarios/${uid}`, {
+            method: 'PUT',
+            body: formData,
+            headers: {
+                'x-token': token
+            }
+        });
+
+        // Verificar si la solicitud fue exitosa
+        if (response.ok) {
+            return await response.json();
+        } else {
+            // En caso de error, lanzar una excepción con el mensaje de error
+            const errorData = await response.json();
+            throw new Error(errorData.message);
+        }
+    } catch (error) {
+        console.error('Error en la solicitud de actualización de perfil:', error);
+        throw new Error('Error en la solicitud de actualización de perfil');
+    }
+
+}
+
+function togglePasswordFields() {
+    let changePasswordCheckbox = document.querySelector('#changePassword');
+    let passwordFields = document.querySelector('#passwordContent').querySelectorAll('input[type="password"]');
+    let passwordFieldsArray = Array.from(passwordFields);
+
+    for (let i = 0; i < passwordFieldsArray.length; i++) {
+        // Agrega o quita el atributo "required" según el estado del checkbox
+        passwordFieldsArray[i].required = changePasswordCheckbox.checked;
+
+        // Borra el contenido del campo si el checkbox está desmarcado
+        if (!changePasswordCheckbox.checked) {
+            passwordFieldsArray[i].value = '';
+        }
+    }
+}
+
+function DeleteAccount(e) {
+    e.preventDefault();
+
+    const modal = document.querySelector('#delteModal');
+    modal.style.display = 'flex';
+}
+
+
+function eliminarCuenta(e) {
+    e.preventDefault();
+    console.log('cuenta eliminada');
+}
+
+function cerrarModal(e) {
+    e.preventDefault();
+    const modal = document.querySelector('#delteModal');
+    modal.style.display = 'none';
+}
+
--- js/register.js
+++ js/register.js
@@ -1,96 +1,158 @@
 
 
 
-const URL = 'http://localhost:8080/'    
+const URL = 'http://localhost:8080/'
 
 
 document.querySelector('#goHome').addEventListener('click', () => {
     window.location.href = '../index.html';
 });
 
+const boxMessageRequest = document.querySelector('#msg-inform-request');
+const textMessageReq = document.querySelector('#msg-inform-p');
 
 
-function handleFormSubmit() {
-    event.preventDefault(); // Evitar la recarga de la página
 
-    let nombre = document.getElementById('nombre').value;
-    let apellido = document.getElementById('apellido').value;
-    let correo = document.getElementById('correo').value;
+async function handleFormSubmit(e) {
+
+    e.preventDefault(); // Evitar la recarga de la página
+
+    try {
+        const usuario = obtenerDatosDeUsuarioInputs();
+
+        if (usuario) {
+            const respuesta = await apiRequestRegister(usuario);
+
+            if (respuesta.usuario) {
+                sendMessageRequestToUserClient('Usuario creado', false);
+                await loginUserCreated(usuario);
+            }
+
+            if (respuesta.errors) {
+                sendMessageRequestToUserClient(respuesta.errors[0].msg, true);
+            }
+        }
+    } catch (error) {
+        sendMessageRequestToUserClient(`Error interno, comuniquese con soporte.`, true);
+    }
+}
+
+function obtenerDatosDeUsuarioInputs() {
+
     let password = document.getElementById('password').value;
     let confirmPassword = document.getElementById('confirmPassword').value;
 
-    // Validación
-    if (nombre.trim() === '' || apellido.trim() === '' || correo.trim() === '' || password.trim() === '') {
-        alert('Por favor, completa todos los campos.');
-        return; // No ejecutar más código si la validación falla
-    }
-
     // Verificar si las contraseñas coinciden
     if (password !== confirmPassword) {
-        alert('Las contraseñas no coinciden. Por favor, verifica.');
+        sendMessageRequestToUserClient('Las contraseñas no coinciden. Por favor, verifica.', true)
         return; // No ejecutar más código si las contraseñas no coinciden
     }
 
-    // Construir el objeto de usuario con el rol
-    let usuario = {
-        nombre: nombre,
-        apellido: apellido,
-        correo: correo,
-        password: password,
-        rol: 'USER'
-    };
+    let usuario = {};
 
-    // Realizar la solicitud POST
-    fetch(`${URL}api/usuarios`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(usuario)
-    })
-        .then(response => response.json())
-        .then(data => {
-            // Manejar la respuesta
-            if (data.errors) {
-                // Mostrar errores si los hay
-                alert('Error: ' + data.errors[0].msg);
-            } else if (data.usuario) {
-                // Mostrar información del usuario registrado
-                console.log('Usuario registrado:', data.usuario);
-                // Realizar la solicitud POST para iniciar sesión
-                fetch(`${URL}api/auth/login`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        correo: data.usuario.correo,  // Usar el correo del usuario registrado
-                        password: password  // Usar la contraseña del formulario de registro
-                    })
-                })
-                    .then(response => response.json())
-                    .then(loginData => {
-                        // Manejar la respuesta del inicio de sesión
-                        if (loginData.msg) {
-                            // Mostrar mensaje de error si lo hay
-                            alert('Error en el inicio de sesión: ' + loginData.msg);
-                        } else {
-                            // Mostrar información del usuario y el token de inicio de sesión
-                            console.log('Usuario logueado:', loginData.usuario);
-                            console.log('Token de sesión:', loginData.token);
-                            localStorage.setItem('x-token', loginData.token);
-                            window.location.href = '/';
+    usuario.password = confirmPassword;
+    usuario.nombre = document.getElementById('nombre').value;
+    usuario.apellido = document.getElementById('apellido').value;
+    usuario.correo = document.getElementById('correo').value;
+    usuario.rol = 'postulante';
 
-                        }
-                    })
-                    .catch(loginError => {
-                        // Manejar errores de red u otros en el inicio de sesión
-                        console.error('Error en la solicitud de inicio de sesión:', loginError);
-                    });
-            }
-        })
-        .catch(error => {
-            // Manejar errores de red u otros
-            console.error('Error en la solicitud:', error);
+    let checkbox = document.getElementById('registrar_empresa');
+
+    if (checkbox.checked) {
+        usuario.nombreEmpresa = document.getElementById('nombre-empresa').value;
+        usuario.razonSocial = document.getElementById('razon-social').value;
+        usuario.direccion = document.getElementById('direccion').value;
+        usuario.telefono = document.getElementById('telefono').value;
+        usuario.rol = 'empresa';
+    }
+
+    return usuario;
+}
+
+
+
+
+async function apiRequestRegister(objUser) {
+    const usuario = objUser;
+    let url = URL + 'api/usuarios/';
+
+    if (usuario.rol === 'postulante') {
+        url += 'postulante';
+    } else {
+        url += 'empresa';
+    }
+
+    try {
+        // Realizar la solicitud POST para registrar al usuario
+        const response = await fetch(`${url}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuario)
         });
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error en la solicitud:', error);
+        throw new Error('Error en la solicitud');
+    }
+}
+
+async function loginUserCreated(usuario) {
+
+    try {
+        // Realizar la solicitud POST para iniciar sesión
+        const response = await fetch(`${URL}api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                correo: usuario.correo, // Usar el correo del usuario registrado
+                password: usuario.password // Usar la contraseña del formulario de registro
+            })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            localStorage.setItem('x-token', data.token);
+            window.location.href = '/';
+        }
+        else {
+            sendMessageRequestToUserClient(`El usuario fue creado pero hubo un error al intentar loguearse.`, true);
+        }
+
+    } catch (error) {
+        sendMessageRequestToUserClient(`Error interno, comuniquese con soporte.`, true);
+    }
+}
+
+function sendMessageRequestToUserClient(message, errors) {
+    boxMessageRequest.classList.toggle('d-none');
+    textMessageReq.textContent = message;
+
+    textMessageReq.style.color = errors ? 'red' : 'blue';
+
+    setTimeout(() => {
+        boxMessageRequest.classList.toggle('d-none');
+        textMessageReq.textContent = '';
+        textMessageReq.style.color = '';
+    }, 1500);
+}
+
+
+function toggleEmpresaForm() {
+    let checkbox = document.getElementById('registrar_empresa');
+    let formFields = document.querySelectorAll('#form_empresa_content input');
+
+    // Verifica si el checkbox está marcado
+    const isChekedOrNot = checkbox.checked;
+
+    formFields.forEach((field) => {
+        // Establecer el atributo "required" basado en el estado del checkbox
+        field.required = isChekedOrNot;
+    });
+
 }
