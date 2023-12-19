bf7af6c416c493620f97c23147b05756fd8623af
Diego <rijmjada@gmail.com>
2023-12-19 15:42:13 -0300

    changes

--- js/jwt.js
+++ js/jwt.js
@@ -1,7 +1,6 @@
-function parseJwt(token) {
+export function parseJwt(token) {
     let base64Url = token.split('.')[1];
     let base64 = base64Url.replace('-', '+').replace('_', '/');
     return JSON.parse(Buffer.from(base64, 'base64').toString('utf-8'));
 }
 
-module.exports = { parseJwt };
